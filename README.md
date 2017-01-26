da5d5b26a138ff2241b2add25fa2b1c2f1a6011d
Christopher Hesse <christopher.hesse@gmail.com>
2017-01-25 16:05:54 -0800

    Fix typos

--- README.md
+++ README.md
@@ -21,14 +21,14 @@ This port is based directly on the torch implementation, and not on an existing 
 ### Getting Started
 
 ```sh
-# Clone this repo
+# clone this repo
 git clone https://github.com/affinelayer/pix2pix-tensorflow.git
 cd pix2pix-tensorflow
-# Download the CMP Facades dataset http://cmp.felk.cvut.cz/~tylecr1/facade/
+# download the CMP Facades dataset http://cmp.felk.cvut.cz/~tylecr1/facade/
 python tools/download-dataset.py facades
-# Train the model (this may take 1-8 hours depending on GPU, on CPU you will be waiting for a bit)
+# train the model (this may take 1-8 hours depending on GPU, on CPU you will be waiting for a bit)
 python pix2pix.py --mode train --output_dir facades_train --max_epochs 200 --input_dir facades/train --which_direction BtoA
-# Test the model
+# test the model
 python pix2pix.py --mode test --output_dir facades_test --input_dir facades/val --checkpoint facades_train
 ```
 
--- pix2pix.py
+++ pix2pix.py
@@ -360,15 +360,15 @@ def create_model(inputs, targets):
         # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2]
         input = tf.concat_v2([discrim_inputs, discrim_targets], axis=3)
 
-        # layer_1: [batch, 256, 256, in_channels * 2] => [batch * 2, 128, 128, ndf]
+        # layer_1: [batch, 256, 256, in_channels * 2] => [batch, 128, 128, ndf]
         with tf.variable_scope("layer_1"):
             convolved = conv(input, a.ndf, stride=2)
             rectified = lrelu(convolved, 0.2)
             layers.append(rectified)
 
-        # layer_2: [batch * 2, 128, 128, ndf] => [batch * 2, 64, 64, ndf * 2]
-        # layer_3: [batch * 2, 64, 64, ndf * 2] => [batch * 2, 32, 32, ndf * 4]
-        # layer_4: [batch * 2, 32, 32, ndf * 4] => [batch * 2, 31, 31, ndf * 8]
+        # layer_2: [batch, 128, 128, ndf] => [batch, 64, 64, ndf * 2]
+        # layer_3: [batch, 64, 64, ndf * 2] => [batch, 32, 32, ndf * 4]
+        # layer_4: [batch, 32, 32, ndf * 4] => [batch, 31, 31, ndf * 8]
         for i in range(n_layers):
             with tf.variable_scope("layer_%d" % (len(layers) + 1)):
                 out_channels = a.ndf * min(2**(i+1), 8)
@@ -378,7 +378,7 @@ def create_model(inputs, targets):
                 rectified = lrelu(normalized, 0.2)
                 layers.append(rectified)
 
-        # layer_5: [batch * 2, 31, 31, ndf * 8] => [batch * 2, 30, 30, 1]
+        # layer_5: [batch, 31, 31, ndf * 8] => [batch, 30, 30, 1]
         with tf.variable_scope("layer_%d" % (len(layers) + 1)):
             convolved = conv(rectified, out_channels=1, stride=1)
             output = tf.sigmoid(convolved)
